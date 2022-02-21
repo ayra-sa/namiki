c3f52e4b12abe9090319e5f2836e4af051c82164
Macbook Pro <macbookpro@192.168.1.28>
2022-02-21 17:13:51 +0700

    namiki project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,25 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:30,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:4
-        }
-    }
-})
+// Hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+// const hero = document.querySelector(".hero")
+// const main = document.querySelector(".main")
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    // hero.classList.toggle('on')
+    // main.classList.toggle('on')
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+    // hero.classList.remove('on')
+    // main.classList.remove('on')
+}
