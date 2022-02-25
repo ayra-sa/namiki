fa166f26860ec5f790cfd5ae25c7a07e23de82fa
Macbook Pro <macbookpro@192.168.1.4>
2022-02-25 16:58:35 +0700

    update hc

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,51 @@ window.onscroll = () => {
         navbar.classList.remove('nav-colored');
     }
 };
+
+// cookies
+
+const cookies = document.querySelector('.cookies')
+const closeBtn = document.querySelector('.close')
+
+closeBtn ? closeBtn.addEventListener("click", closeBox) : false
+
+function closeBox() {
+    cookies.style.display = 'none';
+}
+
+// faq
+
+const acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling
+    panel.style.display === "block" ? panel.style.display = "none" : panel.style.display = "block"
+    
+  });
+}
+
+
+let cards = document.querySelectorAll('.hc-item')
+    
+function liveSearch() {
+    let search_query = document.getElementById("search").value;
+    
+    //Use innerText if all contents are visible
+    //Use textContent for including hidden elements
+    for (var i = 0; i < cards.length; i++) {    
+        cards[i].textContent.toLowerCase().includes(search_query.toLowerCase()) ? cards[i].classList.remove("is-hidden") : cards[i].classList.add("is-hidden")
+    }
+}
+
+//A little delay
+let typingTimer;               
+let typeInterval = 500;  
+let searchInput = document.getElementById('search');
+
+searchInput.addEventListener('keyup', () => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(liveSearch, typeInterval);
+});
